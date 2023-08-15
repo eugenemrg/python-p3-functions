720f224c41ac74b8c530078e9e15cf26aeb8b71d
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-08-15 13:14:50 +0300

    Completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -4,10 +4,10 @@ def greet_programmer():
     print ("Hello, programmer!")
 
 def greet(name):
-    print ("Hello, "+name+"!")
+    print (f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    print ("Hello, "+name+"!")
+    print (f"Hello, {name}!")
 
 def add(num1, num2):
     return num1 + num2
